896f769de3bd57b75d3e33f0366129e18a4ee80d
Wolfgang Mayr <w.mayr@cubido.at>
2019-02-12 08:41:39 +0100

    Added return value to IPersistence.InsertOrUpdate()

--- src/AutoMapper.Collection.EntityFrameworkCore/IPersistence.cs
+++ src/AutoMapper.Collection.EntityFrameworkCore/IPersistence.cs
@@ -2,7 +2,7 @@
 
 namespace AutoMapper.EntityFrameworkCore
 {
-    public interface IPersistence
+    public interface IPersistence<TTo>
     {
         /// <summary>
         /// Insert Or Update the <see cref="T:System.Data.Entity.DbSet`1"/> with <paramref name="from"/>
@@ -10,7 +10,7 @@ namespace AutoMapper.EntityFrameworkCore
         /// <remarks>Uses <see cref="AutoMapper.EquivalencyExpression.EquivalentExpressions.GenerateEquality"/>> to find equality between Source and From Types to determine if insert or update</remarks>
         /// <typeparam name="TFrom">Source Type mapping from</typeparam>
         /// <param name="from">Object to update to <see cref="T:System.Data.Entity.DbSet`1"/></param>
-        void InsertOrUpdate<TFrom>(TFrom from) where TFrom : class;
+        TTo InsertOrUpdate<TFrom>(TFrom from) where TFrom : class;
 
         /// <summary>
         /// Insert Or Update the <see cref="T:System.Data.Entity.DbSet`1"/> with <paramref name="from"/>
@@ -18,7 +18,7 @@ namespace AutoMapper.EntityFrameworkCore
         /// <remarks>Uses <see cref="AutoMapper.EquivalencyExpression.EquivalentExpressions.GenerateEquality"/>> to find equality between Source and From Types to determine if insert or update</remarks>
         /// <param name="type">Source Type mapping from</param>
         /// <param name="from">Object to update to <see cref="T:System.Data.Entity.DbSet`1"/></param>
-        void InsertOrUpdate(Type type, object from);
+        TTo InsertOrUpdate(Type type, object from);
 
         /// <summary>
         /// Remove from <see cref="T:System.Data.Entity.DbSet`1"/> with <paramref name="from"/>
